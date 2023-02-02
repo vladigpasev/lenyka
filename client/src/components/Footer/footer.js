36c4d03fb5cd94c76dd93f6bc77eeb77f08ad2f0
Vladimir Pasev <vladimir@fridgella.com>
2023-02-02 18:55:03 +0200

    footer and db config

--- client/src/components/Footer/footer.js
+++ client/src/components/Footer/footer.js
@@ -1,12 +1,23 @@
 import React from "react";
 import "./footer.css";
+import list from "./list.svg";
+import delivery from "./delivery.svg";
+import plus_sign from "./plus_sign.svg";
+import wallet from "./wallet.svg";
+import directoryka from "./directoryka.svg";
 
 function Footer(){
-    return(
-        <div>
-            Hello world xsxsf
-        </div>
-    );
+   return (
+   <div className="Footer">
+     <nav>
+       <img src={list} />
+       <img src={delivery} />
+       <img className="plus" src={plus_sign} />
+       <img src={wallet} />
+       <img src={directoryka} />
+     </nav>
+   </div>
+ );
 }
 
 export default Footer;
--- server/dbconnect.js
+++ server/dbconnect.js
@@ -3,10 +3,14 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
   host: "s.abax.bg",
   user: "lenyka_online",
-  password: "s2E77wf-sCC--ff"
+  password: "s2E77wf-sCC--ff",
+  database: "lenyka_online"
 });
 
 con.connect(function(err) {
   if (err) throw err;
-  console.log("Connected!");
+  con.query("SELECT ID, group_name, group_code, group_MSlink, group_nivel, teacher_name, onlpris FROM groups ORDER BY ID ASC", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
 });
