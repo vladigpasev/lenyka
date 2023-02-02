7afe78caf876a891ffc2d97cc9cc1f4c8d103124
Vladimir Pasev <vladimir@fridgella.com>
2023-02-02 14:32:41 +0200

    ads

---
+++ client/src/components/Footer/footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./footer.css";
+
+function Footer(){
+    return(
+        <div>
+            Hello world xsxsf
+        </div>
+    );
+}
+
+export default Footer;
